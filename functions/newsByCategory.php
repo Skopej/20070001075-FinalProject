87812908b63066e2549372a60b2a66eed5a38dc3
Barış Aktaş <0227823@gmail.com>
2024-06-10 14:35:40 +0300

    -

--- functions/newsByCategory.php
+++ functions/newsByCategory.php
@@ -1,8 +1,11 @@
 <?php
+
 include $_SERVER["DOCUMENT_ROOT"] . "/Services/newService.php";
-$info =getNewsByContent($_POST['content']);
+$info =getNewsByCategory($_POST['categoryName']);
 $data = [];
 foreach ($info as $value){
     $data[] = array($value->getNewsTitle(),$value->getNewsDetail(),$value->getNewsImage(),$value->getNewsDate());
 }
 echo json_encode($data);
+
+//category
